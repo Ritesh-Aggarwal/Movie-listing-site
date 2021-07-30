e1b3ea3f3145c4b52183a063471f1bcab3824e08
Ritesh-Aggarwal <57039447+Ritesh-Aggarwal@users.noreply.github.com>
2021-07-30 21:10:43 +0530

    deploy heroku v3

---
+++ client/build/precache-manifest.951148b14471696ca7e5216314b1c278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5356352963cd3b1b3fd5a97f09c57e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb5855bd77c74ea7402e",
+    "url": "/static/css/2.6788ebe6.chunk.css"
+  },
+  {
+    "revision": "6e787c5715fa9f1dc824",
+    "url": "/static/css/main.2c1038cb.chunk.css"
+  },
+  {
+    "revision": "bb5855bd77c74ea7402e",
+    "url": "/static/js/2.a75f70ef.chunk.js"
+  },
+  {
+    "revision": "24e39d2560e02578b4d572ddcfc5a172",
+    "url": "/static/js/2.a75f70ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e787c5715fa9f1dc824",
+    "url": "/static/js/main.1b279e8a.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
